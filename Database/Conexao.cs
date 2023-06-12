081915fffa1e9d7f7c8213c92518ee24ac8c0b94
Biblioteca <biblioteca3informatica@gmail.com>
2023-06-12 12:11:49 -0400

    Add pastas

---
+++ Database/Conexao.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System_Biblioteca.Database
+{
+    internal class Conexao
+    {
+    }
+}
